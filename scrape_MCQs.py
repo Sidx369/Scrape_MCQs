05fdfcaa292bfdb76e106616db45aaa27bfc9b84
Sidx369 <siddhantramteke369@gmail.com>
2021-10-13 17:24:10 +0530

    added .py program

---
+++ scrape_MCQs.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = 'https://www.learninsta.com/..'
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+page_body = soup.body
+mcqs = page_body.select('p')
+
+items = []
+flag = 1
+
+for i in mcqs[4:-3]:
+    if flag==1:
+        questions = i.text.split('\n')
+        question_no = questions[0][-2]
+        question = questions[1]
+        options = [x[4:].strip() for x in questions[2:]]
+        info = {
+            "question_no": question_no.strip(),
+            "question": question.strip(),
+            "options": options
+        }
+    elif flag==0:
+        answer = i.get_text().strip('')[12:]
+        info["answer"] = answer
+        items.append(info)
+        #print(info)
+    #print('\n')
+    flag = 1-flag
+
+df = pd.DataFrame(items)
+
+print(len(df['options'].to_list()[0]))
+
+option_df = pd.DataFrame(df['options'].to_list(), columns = ['option1', 'option2', 'option3', 'option4'])
+
+answers = df['answer']
+
+df = pd.concat([df.drop(['options', 'answer'], axis=1), option_df, answers], axis=1)
+
+df.to_csv('sheet2.csv')
