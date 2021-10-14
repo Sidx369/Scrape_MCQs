e5a248e140b35f9c170b30b300f6a412679d2301
Sidx369 <siddhantramteke369@gmail.com>
2021-10-14 13:42:26 +0530

    removed errors for true or false questions

--- scrape_MCQs.py
+++ scrape_MCQs.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = 'https://www.learninsta.com/mcq-questions-for-class-12-business-studies-chapter-1/'
+url = 'https://www.learninsta.com/[URL}...'
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
