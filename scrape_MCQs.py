4672d8648201b25a1e2ce0b3352d0ad5468feb7f
Sidx369 <siddhantramteke369@gmail.com>
2021-10-14 13:37:57 +0530

    removed erros for true or false questions

--- scrape_MCQs.py
+++ scrape_MCQs.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = 'https://www.learninsta.com/[url]..'
+url = 'https://www.learninsta.com/mcq-questions-for-class-12-business-studies-chapter-1/'
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -13,9 +13,12 @@ items = []
 flag = 1
 
 for i in mcqs[4:-3]:
+    if str(i.find('span')) != 'None':
+        break
+
     if flag==1:
         questions = i.text.split('\n')
-        question_no = questions[0][-2]
+        question_no = questions[0].split(' ', 1)[1].strip('.')
         question = questions[1]
         options = [x[4:].strip() for x in questions[2:]]
         info = {
@@ -25,11 +28,13 @@ for i in mcqs[4:-3]:
         }
     elif flag==0:
         answer = i.get_text().strip('')[12:]
-        info["answer"] = answer
+        info["answer"] = answer.split("\n",1)[0].strip('.')
         items.append(info)
         #print(info)
     #print('\n')
     flag = 1-flag
+
+        
 
 df = pd.DataFrame(items)
 
