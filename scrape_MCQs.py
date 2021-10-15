391bd7bb63d9ab4fd7b3de58e0ba1cb03760c331
Sidx369 <siddhantramteke369@gmail.com>
2021-10-15 13:50:09 +0530

    removed bug for passage type questions

--- scrape_MCQs.py
+++ scrape_MCQs.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = 'https://www.learninsta.com/[URL}...'
+url = 'https://www.learninsta.com/[URL]...'
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -20,7 +20,16 @@ for i in mcqs[4:-3]:
         questions = i.text.split('\n')
         question_no = questions[0].split(' ', 1)[1].strip('.')
         question = questions[1]
-        options = [x[4:].strip() for x in questions[2:]]
+        #print([x[0].strip() for x in questions[2][0]])
+        #print('\nq1', questions[2])
+        
+        if questions[2][0]!='(':
+            question = question + ' ' + questions[2]
+            options = [x[4:].strip() for x in questions[3:]]
+        else:
+            options = [x[4:].strip() for x in questions[2:]]
+
+        #print('\nq', question)
         info = {
             "question_no": question_no.strip(),
             "question": question.strip(),
@@ -38,9 +47,20 @@ for i in mcqs[4:-3]:
 
 df = pd.DataFrame(items)
 
-print(len(df['options'].to_list()[0]))
+len_options = []
+for i in range(len(df['options'].to_list())):
+    len_options.append(len(df['options'].to_list()[i]))
 
-option_df = pd.DataFrame(df['options'].to_list(), columns = ['option1', 'option2', 'option3', 'option4'])
+print(max(len_options))
+
+if max(len_options)==4:
+    option_list = ['option1', 'option2', 'option3', 'option4']
+elif max(len_options)==3:
+    option_list = ['option1', 'option2', 'option3']
+elif max(len_options)==5:
+    option_list = ['option1', 'option2', 'option3', 'option4', 'option5']
+
+option_df = pd.DataFrame(df['options'].to_list(), columns = option_list)
 
 answers = df['answer']
 
